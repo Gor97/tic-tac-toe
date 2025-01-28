96bb63dd0b985edde4d71253113f7bcd8114e4bb
Gor97 <gorgrigoryangor@gmail.com>
2025-01-27 17:50:10 -0800

    Added images and animations

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "./Cell.css";
+import xImage from '../x.png'; // Import your X image
+import oImage from '../o.png'; // Import your O image
 
 function Cell(props) {
+    let imageSrc = null;
+    let imageClass = 'tic-tac-toe-image'; // Apply base class for styling
+
+    if (props.value === 'X') {
+      imageSrc = xImage;
+      imageClass += ' x-animation'; // Add animation class for X
+    } else if (props.value === 'O') {
+      imageSrc = oImage;
+      imageClass += ' o-animation'; // Add animation class for O
+    }
     return (
-        <div
-            className={`cellWrapper ${
-                props.value !== " "
-                    ? props.value === "X"
-                        ? "redBg"
-                        : "blueBg"
-                    : ""
-            }`}
-            onClick={props.onClick}
-        >
-            {props.value}
+        <div className="cellWrapper col-4 border border-dark cell" onClick={props.onClick}>
+            {imageSrc && <img src={imageSrc} className={imageClass} />}
         </div>
     );
 }
